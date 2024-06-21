5377bae84740368a8c0d65eca315fb168539ff92
Booster39 <abdiame10@yahoo.fr>
2024-06-21 11:51:59 +0200

    mod: clean img

--- ChaTopBack/src/main/java/com/openclassroom/ChaTop/configuration/SpringFoxConfig.java
+++ ChaTopBack/src/main/java/com/openclassroom/ChaTop/configuration/SpringFoxConfig.java
@@ -20,7 +20,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 )*/
 @Configuration
 @EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
   @Bean
   public Docket api() {
